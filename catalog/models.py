ab8429214c11adf6a81eb5a187d9f0526f6fcf25
SixStringsCoder <rshanlon@gmail.com>
2018-08-15 23:19:58 -0700

    Added Models for Author, Book, BookInstance, Genre, Language; added mock data to database via Admin

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,80 @@
 from django.db import models
+from django.urls import reverse
+import uuid # Required for unique book instances
 
 # Create your models here.
+
+class Genre(models.Model):
+    """Model for book genres"""
+    name = models.CharField(max_length=200, help_text='Enter a book genre (e.g. Science Fiction, Mystery, etc.)')
+
+    def __str__(self):
+        """String for representing the Model object; appears in Admin area"""
+        return self.name
+
+class Language(models.Model):
+    name = models.CharField(max_length=25, help_text='Enter, in Roman script, the language the book is written in (e.g. Chinese, Farsi).')
+
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
+    summary = models.TextField(max_length=1000, help_text='Enter a description of the book')
+    isbn = models.CharField('ISBN', max_length=13, help_text='13 Character <a href="https://www.isbn-international.org/content/what-isbn">ISBN number</a>')
+    genre = models.ManyToManyField(Genre, help_text='Select a genre for this book.')
+    lang = models.ForeignKey(Language, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        """define a URL mapping that has the name book-detail, and define an associated view and template"""
+        return reverse('book-detail', args=[str(self.id)])
+
+
+class BookInstance(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text='Unique ID for this particular book across whole library')
+    book = models.ForeignKey(Book, on_delete=models.SET_NULL, null=True)
+    media = models.CharField(max_length=200, default='paperback', help_text='Type of media such as hardback, paperback, audiobook')
+    due_back = models.DateField(null=True, blank=True)
+
+    LOAN_STATUS = (
+        ('m', 'Maintenance'),
+        ('o', 'On loan'),
+        ('a', 'Available'),
+        ('r', 'Reserved'),
+    )
+
+    status = models.CharField(
+        max_length=1,
+        choices=LOAN_STATUS,
+        blank=True,
+        default='m',
+        help_text='Book availability',
+    )
+
+    class Meta:
+        ordering = ['due_back']
+
+    def __str__(self):
+        """Represents the Model object by ID and book title."""
+        return f'{self.id} ({self.book.title})'
+
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    date_of_birth = models.DateField('Born', null=True, blank=True)
+    date_of_death = models.DateField('Died', null=True, blank=True)
+
+    class Meta:
+        ordering = ['last_name', 'first_name']
+
+    def get_absolute_url(self):
+        return reverse('author-detail', args=[str(self.id)])
+
+    def __str__(self):
+        return f'{self.last_name}, {self.first_name}'
