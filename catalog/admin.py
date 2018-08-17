1ed3367a25fa355d9a54b743b451dbf21d43ad19
SixStringsCoder <rshanlon@gmail.com>
2018-08-16 23:33:58 -0700

    Declare inlines Tabular and Stacked to add associated records to Book and Author in Admin

--- catalog/admin.py
+++ catalog/admin.py
@@ -6,16 +6,29 @@ admin.site.register(Genre)
 admin.site.register(Language)
 
 
+class BookInline(admin.StackedInline):
+    model = Book
+    extra = 0
+
+
+class BooksInstanceInline(admin.TabularInline):
+    model = BookInstance
+    extra = 0
+    verbose_name = 'copy of this title'
+
+
 @admin.register(Author)
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
     fields = [('first_name', 'last_name'), ('date_of_birth', 'date_of_death')]  # displayed horizontally when grouped in a tuple
+    inlines = [BookInline]
 
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'display_genre', 'lang')
     list_filter = ('author', 'lang')
+    inlines = [BooksInstanceInline]
 
 
 @admin.register(BookInstance)
