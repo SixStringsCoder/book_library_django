7b44f222f46585145e3f58894c44e6699ec24033
SixStringsCoder <rshanlon@gmail.com>
2018-08-16 22:58:45 -0700

    Adjust Admin view; Organize List Views

--- catalog/admin.py
+++ catalog/admin.py
@@ -4,6 +4,20 @@ from .models import Genre, Language, Book, BookInstance, Author
 # Register your models here.
 admin.site.register(Genre)
 admin.site.register(Language)
-admin.site.register(Book)
-admin.site.register(BookInstance)
-admin.site.register(Author)
+
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre', 'lang')
+    list_filter = ('author', 'lang')
+
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ('id', 'book', 'status', 'due_back')
+    list_filter = ('status', 'due_back')
