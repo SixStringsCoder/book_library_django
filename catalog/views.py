ef8333d2f9032f663516ac59e2524cda119f4152
SixStringsCoder <rshanlon@gmail.com>
2018-08-17 23:39:24 -0700

    Create Home Page with url mapper, view function and html templates; added static directory with CSS file

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from catalog.models import Book, BookInstance, Author, Genre, Language
 
 # Create your views here.
+
+"""View function for home page of site."""
+def index(request):
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+
+    # Available books (status = 'a')
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+
+    num_authors = Author.objects.all().count()
+
+    context = {
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authors,
+    }
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, 'index.html', context=context)
