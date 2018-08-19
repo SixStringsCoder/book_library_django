e83305a647a46cb8c6e8a291e5de241e61b9a924
SixStringsCoder <rshanlon@gmail.com>
2018-08-18 21:34:40 -0700

    add extra genre context to view and index template

--- catalog/views.py
+++ catalog/views.py
@@ -7,17 +7,20 @@ from catalog.models import Book, BookInstance, Author, Genre, Language
 def index(request):
     num_books = Book.objects.all().count()
     num_instances = BookInstance.objects.all().count()
+    num_authors = Author.objects.all().count()
+    num_genres = Genre.objects.count()
 
     # Available books (status = 'a')
     num_instances_available = BookInstance.objects.filter(status__exact='a').count()
 
-    num_authors = Author.objects.all().count()
+
 
     context = {
         'num_books': num_books,
         'num_instances': num_instances,
         'num_instances_available': num_instances_available,
         'num_authors': num_authors,
+        'num_genres': num_genres,
     }
 
     # Render the HTML template index.html with the data in the context variable
