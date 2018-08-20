c0400539fdbb8d28e24ac4cce36560a1d24d13b4
SixStringsCoder <rshanlon@gmail.com>
2018-08-19 22:40:12 -0700

    Used sessions for getting visit counts on index page

--- catalog/views.py
+++ catalog/views.py
@@ -12,6 +12,9 @@ def index(request):
     num_genres = Genre.objects.count()
     # Available books (status = 'a')
     num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+    # Number of visits to this view, as counted in the session variable.
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
 
     context = {
         'num_books': num_books,
@@ -19,6 +22,7 @@ def index(request):
         'num_instances_available': num_instances_available,
         'num_authors': num_authors,
         'num_genres': num_genres,
+        'num_visits': num_visits,
     }
 
     # Render the HTML template index.html with the data in the context variable
