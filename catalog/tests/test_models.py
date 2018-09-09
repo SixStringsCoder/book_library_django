aad531f42bbcb88dd4cd2f9c79416a261b803e83
SixStringsCoder <rshanlon@gmail.com>
2018-09-08 22:05:22 -0700

    Add tests for views using class AuthorListView

--- catalog/tests/test_models.py
+++ catalog/tests/test_models.py
@@ -11,27 +11,27 @@ class AuthorModelTest(TestCase):
     def test_first_name_label(self):
         author = Author.objects.get(id=1)
         field_label = author._meta.get_field('first_name').verbose_name
-        self.assertEquals(field_label, 'first name')
+        self.assertEqual(field_label, 'first name')
 
     def test_last_name_label(self):
         author = Author.objects.get(id=1)
         field_label = author._meta.get_field('last_name').verbose_name
-        self.assertEquals(field_label, 'last name')
+        self.assertEqual(field_label, 'last name')
 
     def test_date_of_birth_label(self):
         author = Author.objects.get(id=1)
         field_label = author._meta.get_field('date_of_birth').verbose_name
-        self.assertEquals(field_label, 'Born')
+        self.assertEqual(field_label, 'Born')
 
     def test_date_of_death_label(self):
         author = Author.objects.get(id=1)
         field_label = author._meta.get_field('date_of_death').verbose_name
-        self.assertEquals(field_label, 'Died')
+        self.assertEqual(field_label, 'Died')
 
     def test_first_name_max_length(self):
         author = Author.objects.get(id=1)
         max_length = author._meta.get_field('first_name').max_length
-        self.assertEquals(max_length, 100)
+        self.assertEqual(max_length, 100)
 
     def test_last_name_max_length(self):
         author = Author.objects.get(id=1)
@@ -41,12 +41,12 @@ class AuthorModelTest(TestCase):
     def test_object_name_is_last_name_comma_first_name(self):
         author = Author.objects.get(id=1)
         expected_object_name = f'{author.last_name}, {author.first_name}'
-        self.assertEquals(expected_object_name, str(author))
+        self.assertEqual(expected_object_name, str(author))
 
     def test_get_absolute_url(self):
         author = Author.objects.get(id=1)
         # This will also fail if the urlconf is not defined.
-        self.assertEquals(author.get_absolute_url(), '/catalog/author/1')
+        self.assertEqual(author.get_absolute_url(), '/catalog/author/1')
 
 
 class BookModelTest(TestCase):
@@ -57,47 +57,47 @@ class BookModelTest(TestCase):
     def test_title_label(self):
         title = Book.objects.get(id=1)
         field_label = title._meta.get_field('title').verbose_name
-        self.assertEquals(field_label, 'title')
+        self.assertEqual(field_label, 'title')
 
     def test_author_label(self):
         author = Book.objects.get(id=1)
         field_label = author._meta.get_field('author').verbose_name
-        self.assertEquals(field_label, 'author')
+        self.assertEqual(field_label, 'author')
 
     def test_summary_label(self):
         summary = Book.objects.get(id=1)
         field_label = summary._meta.get_field('summary').verbose_name
-        self.assertEquals(field_label, 'summary')
+        self.assertEqual(field_label, 'summary')
 
     def test_isbn_label(self):
         isbn = Book.objects.get(id=1)
         field_label = isbn._meta.get_field('isbn').verbose_name
-        self.assertEquals(field_label, 'ISBN')
+        self.assertEqual(field_label, 'ISBN')
 
     def test_genre_label(self):
         genre = Book.objects.get(id=1)
         field_label = genre._meta.get_field('genre').verbose_name
-        self.assertEquals(field_label, 'genre')
+        self.assertEqual(field_label, 'genre')
 
     def test_lang_label(self):
         lang = Book.objects.get(id=1)
         field_label = lang._meta.get_field('lang').verbose_name
-        self.assertEquals(field_label, 'lang')
+        self.assertEqual(field_label, 'lang')
 
     def test_title_max_length(self):
         title = Book.objects.get(id=1)
         max_length = title._meta.get_field('title').max_length
-        self.assertEquals(max_length, 200)
+        self.assertEqual(max_length, 200)
 
     def test_summary_max_length(self):
         summary = Book.objects.get(id=1)
         max_length = summary._meta.get_field('summary').max_length
-        self.assertEquals(max_length, 1000)
+        self.assertEqual(max_length, 1000)
 
     def test_isbn_max_length(self):
         isbn = Book.objects.get(id=1)
         max_length = isbn._meta.get_field('isbn').max_length
-        self.assertEquals(max_length, 13)
+        self.assertEqual(max_length, 13)
 
 
 class BookInstanceModelTest(TestCase):
@@ -108,17 +108,17 @@ class BookInstanceModelTest(TestCase):
     def test_bookinstance_id_help_text(self):
         id = BookInstance.objects.get(id='04a77f53-172c-4579-9a39-64e7cc30fd66')
         help_text = id._meta.get_field('id').help_text
-        self.assertEquals(help_text, 'Unique library ID for this book')
+        self.assertEqual(help_text, 'Unique library ID for this book')
 
     def test_bookinstance_media_help_text(self):
         media = BookInstance.objects.get(id='04a77f53-172c-4579-9a39-64e7cc30fd66')
         help_text = media._meta.get_field('media').help_text
-        self.assertEquals(help_text, 'Type of media (e.g. hardback, audiobook)')
+        self.assertEqual(help_text, 'Type of media (e.g. hardback, audiobook)')
 
     def test_bookinstance_max_lengthl(self):
         media = BookInstance.objects.get(id='04a77f53-172c-4579-9a39-64e7cc30fd66')
         max_length = media._meta.get_field('media').max_length
-        self.assertEquals(max_length, 200)
+        self.assertEqual(max_length, 200)
 
 
 class GenreModelTest(TestCase):
@@ -129,12 +129,12 @@ class GenreModelTest(TestCase):
     def test_genre_name_max_length(self):
         genre = Genre.objects.get(id=1)
         max_length = genre._meta.get_field('name').max_length
-        self.assertEquals(max_length, 200)
+        self.assertEqual(max_length, 200)
 
     def test_genre_name_help_text(self):
         name = Genre.objects.get(id=1)
         help_text = name._meta.get_field('name').help_text
-        self.assertEquals(help_text, 'Enter a book genre (e.g. Science Fiction, Mystery, etc.)')
+        self.assertEqual(help_text, 'Enter a book genre (e.g. Science Fiction, Mystery, etc.)')
 
 
 class LanguageModelTest(TestCase):
@@ -150,4 +150,4 @@ class LanguageModelTest(TestCase):
     def test_langauge_name_help_text(self):
         name = Language.objects.get(id=1)
         help_text = name._meta.get_field('name').help_text
-        self.assertEquals(help_text, 'Enter the language the book is written in (e.g. Chinese, Farsi).')
+        self.assertEqual(help_text, 'Enter the language the book is written in (e.g. Chinese, Farsi).')
--- catalog/tests/test_views.py
+++ catalog/tests/test_views.py
@@ -1,36 +1,43 @@
-import datetime
-
 from django.test import TestCase
-from django.utils import timezone
+from django.urls import reverse
 
-from catalog.forms import RenewBookForm
+from catalog.models import Author
 
+class AuthorListViewTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        number_of_authors = 13
 
-class RenewBookFormTest(TestCase):
-    def test_renew_form_date(self):
-        form = RenewBookForm()
-        self.assertTrue(form.fields['renewal_date'].label == None or form.fields['renewal_date'].label == 'renewal_date')
+        for author_id in range(number_of_authors):
+            Author.objects.create(
+                first_name=f'Shirley {author_id}',
+                last_name=f'UbeeKiddin {author_id}',
+            )
 
-    def test_renew_form_date_field_help_text(self):
-        form = RenewBookForm()
-        self.assertEquals(form.fields['renewal_date'].help_text, 'Enter a date between now and 4 weeks (default 3).')
+    def test_view_url_exists_at_desired_location(self):
+        response = self.client.get('/catalog/authors/')
+        self.assertEqual(response.status_code, 200)
 
-    def test_renew_form_date_in_past(self):
-        date = datetime.date.today() - datetime.timedelta(days=1)
-        form = RenewBookForm(data={'renewal_date': date})
-        self.assertFalse(form.is_valid())
+    def test_view_url_accessible_by_name(self):
+        response = self.client.get(reverse('authors'))
+        self.assertEqual(response.status_code, 200)
 
-    def test_renew_form_date_too_far_in_future(self):
-        date = datetime.date.today() + datetime.timedelta(weeks=4, days=1)
-        form = RenewBookForm(data={'renewal_date': date})
-        self.assertFalse(form.is_valid())
+    def test_view_uses_correct_template(self):
+        response = self.client.get(reverse('authors'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'catalog/author_list.html')
 
-    def test_renew_form_date_today(self):
-        date = datetime.date.today()
-        form = RenewBookForm(data={'renewal_date': date})
-        self.assertTrue(form.is_valid())
+    def test_pagination_is_ten(self):
+        response = self.client.get(reverse('authors'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue('is_paginated' in response.context)
+        self.assertTrue(response.context['is_paginated'] == True)
+        self.assertTrue(len(response.context['author_list']) == 10)
 
-    def test_renew_form_date_max(self):
-        date = timezone.now() + datetime.timedelta(weeks=4)
-        form = RenewBookForm(data={'renewal_date': date})
-        self.assertTrue(form.is_valid())
+    def test_lists_all_authors(self):
+        # Get second page and confirm it has (exactly) remaining 3 items
+        response = self.client.get(reverse('authors')+'?page=2')
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue('is_paginated' in response.context)
+        self.assertTrue(response.context['is_paginated'] == True)
+        self.assertTrue(len(response.context['author_list']) == 3)
